00f41a8f181a2b54c4d943fd4ed4ee12db9700cd
Felipe-Velez <111096461+Felipe-Velez@users.noreply.github.com>
2022-08-16 19:09:54 -0600

    Add files via upload

---
+++ Felipe-Velez.github.io-main/README.md
@@ -0,0 +1,1 @@
+# Felipe-Velez.github.io
---
+++ Felipe-Velez.github.io-main/Recursamiento-main/Recursamiento-main/js/NetCETIS.js
@@ -0,0 +1,14 @@
+NetCETIS.js
+
+	$( document).ready(function(){
+		$("#like").click(function(){
+		var likes = $("reactions").text();
+		likes++;
+		$("#reactions").text(likes);
+	});
+
+		
+		$("#comment").click(function(){
+		$("#comentario").toggle("slow");
+	});
+});
